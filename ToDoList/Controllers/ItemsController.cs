fde4356a8b4621542f8ecfcafeaa9a0d5fc05993
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-29 14:23:14 -0500

    add all basic functionaliity with a few errors
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -16,12 +18,14 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
+      // for each Item in the database include the category it belongs to and then put into list
     }
 
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
@@ -37,6 +41,34 @@ namespace ToDoList.Controllers
     {
       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
       return View(thisItem);
+    }
+    public ActionResult Edit (int id);  // Get method that routes to page with form to update item
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id)
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisItem); // finds spectific item and passes it to the view
+    }
+    [HttpPost]
+    public ActionResult Edit (Item item) // updates the item after being routed to there by Get method
+    {
+      _db.Entry(item).State = EntityState.Modified;  // (item) = routing parameter // Entry () = method, State = property // state updated to EntityState.Modified = tells Entity the entry has been updated 
+      _db.SaveChanges();// once state has been modified, we call SaveChanges
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete (int id) // initializes delete process by grabbing the item?
+    {
+      var thisItem = _db.Items.FirstOrDefault(item => items.ItemId == id);
+      return View (thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed (int id) // confirms the delete of selected Id
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId ==id);
+      _db.Items.Remove(thisItem); // destroys entry
+      _db.SaveChanges(); // commits to database
+      return RedirectToAction("Index"); // returns to top
     }
   }
 }
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using ToDoList.Models; 
 
 namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public virtual DbSet<Category> Categories {get; set;} //we need virtual keyword here to enable lazy loading
     public DbSet<Item> Items { get; set; }
+   
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
   }
